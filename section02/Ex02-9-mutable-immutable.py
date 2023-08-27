aab8ed856195250827670d8e0c9ac54016e19f06
ITSC <sca@koreaedugroup.com>
2023-08-27 17:20:11 +0900

    mutable-immutable

--- section02/Ex02-9-mutable-immutable.py
+++ section02/Ex02-9-mutable-immutable.py
@@ -6,8 +6,35 @@ mutable - 메모리 값을 변경 가능 객체
 '''
 me = [1, 2, 3]
 print(me)
+print(id(me))
 
-me.
+me.append(4)
+print(me)
+print(id(me))
 
+'''
+immutable - 메모리 값 변경 불가
+    정수(int), 실수(float), 문자열(str), 튜플(tuple)
+'''
+imme = 10
+print(imme)
+print(id(imme))
 
+imme += 1    # imme = imme + 1
+print(imme)
+print(id(imme))
 
+imme2 = 10
+print(imme2)
+print(id(imme2))
+
+# 튜플 id 값 확인하기
+thistuple = ('피카츄', '라이츄', '파이리')
+print(thistuple)
+print(id(thistuple))
+#casting(형변환) ['피카츄', '라이츄', '파이리']
+thiscast = list(thistiple)
+thiscast[1] = '잠만보'
+thistuple = tuple(thiscast)   #('피카츄', '라이츄', '파이리')
+print(thistuple)
+print(id(thistuple))
